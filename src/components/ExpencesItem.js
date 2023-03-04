812d4feeff83833321412e84a6d1c2be4a62a61a
Yulii Shtel <ulik1shtel@gmail.com>
2023-03-04 13:42:22 +0200

    Writing More Complex JSX Code

--- src/components/ExpencesItem.js
+++ src/components/ExpencesItem.js
@@ -1,5 +1,13 @@
 function ExpenceItem() {
-  return <h2>Expence item!</h2>;
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenceItem;
